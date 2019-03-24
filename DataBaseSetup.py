ea6c44c525469ca97798b93888bae471351f87a0
Ruihan Wei <31101321+RuihanWei@users.noreply.github.com>
2019-03-24 15:56:29 -0400

    sample labelling

---
+++ DataBaseSetup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import MySQLdb
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="",
+  passwd=""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase")
--- ObjectVersionControl.py
+++ ObjectVersionControl.py
@@ -1,8 +1,9 @@
 import cv2
 from darkflow.net.build import TFNet
 import matplotlib.pyplot as plt
+import MySQLdb
 
-# credit https://github.com/markjay4k/YOLO-series/blob/master/part2%20-%20Processing%20Images%20with%20YOLO%20and%20openCV.ipynb
+# options and image read/cast credit https://github.com/markjay4k/YOLO-series/blob/master/part2%20-%20Processing%20Images%20with%20YOLO%20and%20openCV.ipynb
 
 
 options = {
@@ -18,14 +19,13 @@ tfnet = TFNet(options)
 # read the color image and covert to RGB
 
 
-img = cv2.imread('sample_computer.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('Samplesetup2.jpg', cv2.IMREAD_COLOR)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # use YOLO to predict the image
 results = tfnet.return_predict(img)
 
 img.shape
-
 # pull out some info from the results
 
 for result in results:
@@ -34,10 +34,9 @@ for result in results:
     label = result['label']
     print(result['label'] + " confidence: " + str(result['confidence']))
 
-# add the box and label and display it
+    # add the box and label and display it
     img = cv2.rectangle(img, tl, br, (0, 255, 0), 7)
     img = cv2.putText(img, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
-
 
 plt.imshow(img)
 plt.show()
